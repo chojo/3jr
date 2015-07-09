e92c09b2f4af4533e1be2df676bff025f2f0fd14
Joon Ki Choi <choi@zaphod.local>
2015-07-09 12:30:12 +0200

    fix: calculator-spec.js, test green

--- spec/javascripts/calculator-spec.js
+++ spec/javascripts/calculator-spec.js
@@ -1,10 +1,21 @@
-var calculator = require("calculator.js");
-
-describe("multiplication", function() {
-
-  it("should multiply 2 and 3", function () {
-    var product = calculator.multiply(2, 3);
-    expect(product).toBe(6);
+describe("require", function() {
+  beforeEach(function() {
+    try {
+      fs = require('fs');
+      calculatorFs = fs.readFileSync('calculator.js', 'utf-8');
+    } catch (e) {
+      console.log('File not found!');
+    }
   });
 
+
+  describe("multiplication", function() {
+
+    it("should multiply 2 and 3", function () {
+      eval(calculatorFs);
+      var product = multiply(2, 3);
+      expect(product).toBe(6);
+    });
+
+  });
 });
--- spec/javascripts/calculator.js
+++ spec/javascripts/calculator.js
@@ -1,4 +1,8 @@
+/*
 exports.multiply = function (multiplier1, multiplier2) {
-
 };
+*/
 
+var multiply = function(multiplier1, multiplier2) {
+  return multiplier1 * multiplier2;
+}
